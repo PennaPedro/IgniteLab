27d2564b5e6c3217bb0bca43a78b1c7cb0e37090
PennaPedro <pennapedro@hotmail.com>
2022-07-20 20:54:10 -0300

    first commit

---
+++ src/Routers.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes, useParams } from "react-router-dom";
+import { Event } from "./pages/Event";
+import { Subscribe } from "./pages/Subscriber";
+
+export function Router () {
+    return (
+        <Routes>
+            <Route path="/" element={<Subscribe />}/>
+            <Route path="/event" element={<Event/>}/>
+            <Route path="/event/lesson/:slug" element={<Event/>}/>
+        </Routes>
+    )
+}
